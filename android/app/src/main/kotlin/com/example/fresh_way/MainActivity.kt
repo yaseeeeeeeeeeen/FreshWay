a4d9f656a03b5e141e70be6fd15731b39ba5fbff
yaseeeeeeeeeeen <muhammardyaseen@gmail.com>
2024-11-13 18:03:50 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/fresh_way/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.fresh_way
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:fresh_way/my_app.dart';
+
+void main(){
+  runApp(MyApp());
+}
